91c68eb9261554cdbcab29622e797ae78fb24c75
antony-wilson <antony.wilson@stfc.ac.uk>
2015-12-01 16:15:41 +0000

    add pagination to the list of doi's

--- doi_site/views.py
+++ doi_site/views.py
@@ -11,40 +11,57 @@ from django.views.generic import View
 from django.views.generic.list import ListView
 
 from doi_site import settings
+from doi_site.exception import ExternalError
 from doi_site.settings import DATACITE_HANDLER, DATACITE_TEST_URL, \
     DATACITE_URL, DOI_PREFIX, ORGANISATION_NAME, ORGANISATION_DOI_EMAIL
 from mds.http.get import get as _get
-from mds.http.helper import get_accept_header
 from mds.models import GroupProfile
 
 
-class DoiList(View):
+# pylint: disable=too-many-ancestors
+class DoiList(ListView):
     """
     Display all the DOIs for this organisation.
 
     """
+    paginate_by = 25
+    template_name = 'doi_site/dois.html'
+    context_object_name = 'dois'
 
     def dispatch(self, *args, **kwargs):
         return super(DoiList, self).dispatch(*args, **kwargs)
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
+        try:
+            return super(DoiList, self).get(request, *args, **kwargs)
+        except ExternalError as ex:
+            context = {'message': ex.message}
+            context['is_testing'] = _is_test_url()
+            return render(request, 'doi_site/error.html', context)
+
+    def get_queryset(self):
         """
         Get the list of DOIs.
 
         """
         url = urljoin(DATACITE_URL, "doi")
-        context = {'is_testing' : _is_test_url()}
-        response = _get(request.method, url, get_accept_header(request))
+        response = _get('GET', url, {})
         if response.status_code != 200:
-            context['message'] = response.content
-            return render(request, 'doi_site/error.html', context)
-        context['handler'] = DATACITE_HANDLER
+            raise ExternalError(response.content)
         dois = []
         for line in response:
             if line != '':
-                dois.append(line.strip())
-        context['dois'] = dois
-        return render(request, 'doi_site/dois.html', context)
+                dois.append(line.strip().decode('ISO-8859-1'))
+        return dois
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(DoiList, self).get_context_data(**kwargs)
+        context['is_testing'] = _is_test_url()
+        context['handler'] = DATACITE_HANDLER
+        context['doi_prefix'] = DOI_PREFIX
+        context['search'] = DATACITE_URL
+        return context
 
 
 class HomeView(View):
