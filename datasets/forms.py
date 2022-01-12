ebb4deb1744d3130efeb6d8d6fbd1b0eeee1dba3
unknown <albertopopescu@outlook.com>
2022-01-12 18:32:55 +0000

    form +/- is working within the wrapper

--- datasets/forms.py
+++ datasets/forms.py
@@ -1,57 +1,13 @@
 from django import forms
 from django.core.validators import RegexValidator
-from django.forms import MultiWidget, TextInput
-
+from django.forms import formset_factory, MultiWidget, TextInput
 RT_CHOICES= [
     ('type 1', 'Type A'),
     ('type 2', 'Type B'),
     ('type 3', 'Type C'),
     ('type 4', 'Type D'),
     ]
-
-    
-class CreatorField(forms.MultiValueField):
-    def __init__(self, **kwargs):
-        # Define one message for all fields.
-        error_messages = {
-            'incomplete': 'Enter a country calling code and a phone number.',
-        }
-        # Or define a different message for each field.
-        fields = (
-            forms.CharField(
-                error_messages={'incomplete': 'Enter a country calling code.'},
-                validators=[
-                    RegexValidator(r'^[0-9]+$', 'Enter a valid country calling code.'),
-                ],
-                label = "Given Name"
-            ),
-            forms.CharField(
-                error_messages={'incomplete': 'Enter a phone number.'},
-                validators=[RegexValidator(r'^[0-9]+$', 'Enter a valid phone number.')],
-            ),
-            forms.CharField(
-                validators=[RegexValidator(r'^[0-9]+$', 'Enter a valid extension.')],
-                required=False,
-            ),
-        )
-        super().__init__(
-            error_messages=error_messages, fields=fields,
-            require_all_fields=False, **kwargs
-        )
-
-class CreatorWidget(MultiWidget):
-    template_name = 'creator.html'
-
-    def __init__(self, attrs=None):
-        widgets=[forms.TextInput, forms.TextInput, forms.TextInput]
-        super().__init__(widgets, attrs)
-    
-    def decompress(self, value):
-        if value:
-            pass
-        return [None, None, None]
-
-class SimpleForm(forms.Form):
+class DoiForm(forms.Form):
     identifier = forms.CharField(label='Identifier', max_length=100)
     creator_given_name = forms.CharField(label='Given Name', max_length=100, widget=forms.TextInput(attrs={'class' : 'creator'}))
     creator_family_name = forms.CharField(label='Family Name', max_length=100, widget=forms.TextInput(attrs={'class' : 'creator'}))
@@ -61,5 +17,17 @@ class SimpleForm(forms.Form):
     publication_year = forms.CharField(max_length=100)
     resource_type= forms.CharField(label='Pick the resource type:', widget=forms.Select(choices=RT_CHOICES))
     subject = forms.CharField(max_length=100)
-    creator = CreatorField(label='', widget = CreatorWidget())
-    
+class SubjectForm(forms.Form):
+     subject = forms.CharField(label='Subject')
+SubjectFormset = formset_factory(SubjectForm, extra=1)
+class CreatorForm(forms.Form):
+    givenname = forms.CharField(label='Given Name')
+    familyname = forms.CharField(label='Family Name')
+    affiliation = forms.CharField(label='Affiliation')
+   
+CreatorFormset = formset_factory(CreatorForm, extra=1)
+
+
+
+
+
--- datasets/views.py
+++ datasets/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
-from .forms import SimpleForm
+from .forms import SubjectFormset, CreatorFormset, DoiForm
+from django.shortcuts import render, redirect
 
 
 from doi_site.settings import DATACITE_TEST_URL, DATACITE_URL
@@ -15,13 +16,42 @@ class Mint(View):
 
     # pylint: disable=no-self-use
     def get(self, request):
-        form = SimpleForm()
-        context = {"is_testing": _is_test_url(), 'form': form}
-        return render(request, "datasets/mint.html", context)
+        template_name = 'create_normal.html'
+        heading_message = 'Formset Demo'
+        form = DoiForm(request.GET or None)
+        formset1 = SubjectFormset(request.GET or None, prefix='form1')
+        formset2 = CreatorFormset(request.GET or None, prefix='form2')
+        return render(request, template_name, {
+        'form': form,
+        'formset1': formset1,
+        'formset2': formset2,
+        'heading': heading_message,
+    })
 
-
+    def post(self, request):
+        formset1 = SubjectFormset(request.POST, prefix='form1')
+        formset2 = CreatorFormset(request.POST, prefix='form2')
+        template_name = 'create_normal.html'
+        heading_message = 'Formset Demo'
+        if formset1.is_valid() and formset2.is_valid():
+            for form in formset1:
+                # extract name from each form and save
+                name = form.cleaned_data.get('name')
+                print(name)
+            for form in formset2:
+                # extract name from each form and save
+                creator = form.cleaned_data.get('creator')
+                print(creator)
+        return render(request, template_name, {
+        'formset1': formset1,
+        'formset2': formset2,
+        'heading': heading_message,
+    })
 def _is_test_url():
     if DATACITE_URL == DATACITE_TEST_URL:
         return True
     return False
+
+   
+
 
