f0ca04dadfc2c71e6b2e0702d57095fcc4ea3800
Antony Wilson <antony.wilson@stfc.ac.uk>
2022-06-28 16:55:11 +0100

    add check for existing DOI back in to form

--- datasets/forms.py
+++ datasets/forms.py
@@ -133,8 +133,8 @@ class DoiForm(forms.Form):
         
         if identifier in self._domains:
             self._errors['identifier'] = self.error_class(['Cannot use Sub-Domain by itself, please append with unique identifiying data'])
-        # elif r.status_code == 200:
-        #     self._errors['identifier'] = self.error_class(['DOI is already registered, please use another'])
+        elif r.status_code == 200:
+            self._errors['identifier'] = self.error_class(['DOI is already registered, please use another'])
 
 class SubjectForm(forms.Form):
      subject = forms.CharField(label='Subject', required=False, widget=forms.TextInput(attrs={'placeholder': 'Subject', 'class':'form-control form-control-sm', 'id':'subject'}))
